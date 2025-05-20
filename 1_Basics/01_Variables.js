4399c19004fe14f950da0804cac97a47e00c02f8
AbhishekSH807 <abhishekshooli101@gmail.com>
2025-05-20 02:57:19 +0000

    commit variables

---
+++ 1_Basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "abhi23@gmail.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hgd@gmail.com"
+accountPassword = "234321"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
