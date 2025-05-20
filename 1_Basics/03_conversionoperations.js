fe67cac44d6a78b65e1008bdf0eb6c6d8270385c
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-20 13:31:40 +0000

    commit

---
+++ 1_Basics/03_conversionoperations.js
@@ -0,0 +1,9 @@
+let score = null // 0 33 undefined
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
