4fe8dc8af1fd44ebb1f66f2124d0eb04fbac66ca
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-20 12:33:51 +0000

    commit

--- 1_Basics/01_Variables.js
+++ 1_Basics/01_Variables.js
@@ -8,6 +8,7 @@ accountCity = "Jaipur"
 accountEmail = "hgd@gmail.com"
 accountPassword = "234321"
 accountCity = "Bengaluru"
+let accountState;
 
 console.log(accountId);
 
@@ -16,4 +17,4 @@ Prefer not to use var
 Because of issue in block scope and functional scope
 */
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 1_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  // we  are using node js not brpwser
+
+console.log(3
+    +
+    3) // code redability should be high
+console.log("Hitesh")
+
+
+
+let name = "Abhishek"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined =>
+// symbol =. unique
+
+//object
+
+
+console.log(typeof null) // object
+
+console.log(typeof undefined) // undefined
+
