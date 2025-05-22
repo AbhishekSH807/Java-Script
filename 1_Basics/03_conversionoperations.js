2f23040b900b9631e3b997734bd8ef0d50752098
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-22 12:55:13 +0000

    commit

--- 1_Basics/03_conversionoperations.js
+++ 1_Basics/03_conversionoperations.js
@@ -1,9 +1,20 @@
-let score = null // 0 33 undefined
+//let score = null // 0 33 undefined "33abc" true/false
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
