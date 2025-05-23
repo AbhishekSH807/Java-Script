37106c4abeff5d1ce4cb75f4f34f33040fc92e97
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-23 13:22:51 +0000

    commit

--- 1_Basics/03_conversionoperations.js
+++ 1_Basics/03_conversionoperations.js
@@ -31,12 +31,24 @@
 
 // ********************** Operations **********************
 
-let value = 3
-let negValue = -value
+//let value = 3
+//let negValue = -value
 // console.log(negValue)
 
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 ="hello"
+let str2 =" Abhi"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
 
 
