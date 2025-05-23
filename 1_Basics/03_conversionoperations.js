4ee058de07847288b2a17dbeb656e97a3d2fd999
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-23 11:37:32 +0000

    commit

--- 1_Basics/03_conversionoperations.js
+++ 1_Basics/03_conversionoperations.js
@@ -23,9 +23,20 @@
 // "abhi" => true
 
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// ********************** Operations **********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+
 
