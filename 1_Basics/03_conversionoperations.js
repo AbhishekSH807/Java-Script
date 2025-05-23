27c7ec54bdbc4b8f7ce6c557982c1c28a3677088
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-23 11:18:27 +0000

    conversion of data

--- 1_Basics/03_conversionoperations.js
+++ 1_Basics/03_conversionoperations.js
@@ -13,8 +13,19 @@
 // true => 1; false => 0
 
 
-let isLoggedIn = 1
+//let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "abhi" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
 
