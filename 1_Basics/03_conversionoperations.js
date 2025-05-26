345a828781b84fb4567cc1f81f690cc9dfe45e17
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-26 17:34:17 +0000

    commit

--- 1_Basics/03_conversionoperations.js
+++ 1_Basics/03_conversionoperations.js
@@ -55,6 +55,28 @@
 // console.log( 3 + 4 * 5 % 3)
 // console.log( (3 + 4) * (5 % 3))
 
-console.log(+true);
+// console.log(+true);
+// console.log(+"");
 
+
+// let num1, num2, num3
+
+// num1 = num2 = num3=2+2
+
+// let gameCounter = 100
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
 
