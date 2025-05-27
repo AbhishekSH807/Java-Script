a1e20ebd7bea5afc5f5751ace682a4bcd5742faf
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-27 17:07:08 +0000

    commit

---
+++ Datatypes_Summary.js
@@ -0,0 +1,24 @@
+// Primitive : call by value
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score:Number = 100
+const score = 100 // number
+const scoreValue = 100.3 // number
+
+const isLoggedIn = false // Boolean
+const outsideTemp = null // null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId); // not equal
+
+const bigNumber = 3456788887654334567n // use n in last
+
+// Non-primitive (Reference type): call bye Reference(memory reference is allowed)
+
+// Array, Objects, Functions
+
+const heros = ["Hanuman", "Ram"]
