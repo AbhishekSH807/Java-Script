460e3d7ba546bc5c22230bfa29dfa5a53c2c9e53
Abhishek Sanganabasappa Hooli <abhishekshooli101@gmail.com>
2025-05-27 16:30:01 +0000

    commit

---
+++ 1_Basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 <= 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// compare same datatypes
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// equality check " == "   // omaprisions " >= <= < >"
+// comparision convert null to a number treat it as 0
+// so null >= 0 is true and null > 0 is false
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+
+
+// === strict check    also check data type
+
+console.log("2" === 2);
