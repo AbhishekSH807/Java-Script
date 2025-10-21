f8c20c6dd05cf9de90d7dbefb041bd55f1a86027
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-10-21 18:30:15 +0000

    commit control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,4 +26,32 @@ if (userEmail.length === 0) {
 
 const emptyObj = {}
 
-if 
+if (Object.keys(emptyObj).length === 0) {
+  console.log("Object is empty");
+  
+}
+
+/* in console of browser
+false == 0
+false == ''
+0 == ''
+ all gives true
+ */
+
+ // Nullish Coalescing Operator (??): null undefined
+
+ let val1;
+
+ //  val1 = 5 ?? 10  // 5
+// val1 = null ?? 10  // 10
+// val1 = undefined ?? 15 // 15
+val1 = null ?? 10 ?? 20  // 10
+
+console.log(val1); 
+
+// Terniary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less tham 80")  : console.log("more then 80");
