95b903ddd1759b22276fc66f77268fe456c14c39
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-10-21 14:01:44 +0000

    commit

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+// syntax of switch
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+/*
+const month1 = "mar"
+
+switch (month1) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("mar");
+        break;
+    case "mar":
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+*/
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,29 @@
+const userEmail = "abhi@.ai"  // Got user email
+// const userEmail = ""  // Don't have user email
+// const userEmail = []  //  Got user email
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+/*
+
+falsy values :
+  false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+truthy values:
+  "0", 'false', " ", [], {}, function(){}
+*/
+
+/*
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+    
+}
+*/
+
+const emptyObj = {}
+
+if 
