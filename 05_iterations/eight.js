633590a9960bdf6557224cbeafbfb4f2c14c01ba
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-10-22 11:56:08 +0000

    commit reduce

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,8 +1,38 @@
 const myNums = [1, 2, 3]
 
+/*
 const myTotal = myNums.reduce( (acc, curval) => {
     console.log(`acc: ${acc} and curval: ${curval}`);
     
     return acc + curval
-}, 0)
+}, 0) // here you have to give accumalator value
 console.log(myTotal);
+
+*/
+
+const myTotal = myNums.reduce( (acc, curval) => acc + curval ,0)
+
+// console.log(myTotal);
+
+const shoppingCart =[
+    {
+        itemName: "js Course",
+        price: 2999
+    },
+    {
+        itemName: "py Course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev Course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(priceToPay);
