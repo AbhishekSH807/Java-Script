6eb12312a74722d103ee4f0aa8402db19dd8b97c
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-10-22 10:36:50 +0000

    commit

---
+++ 05_iterations/six.js
@@ -0,0 +1,46 @@
+
+
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+/*
+// for each doesn't return values means if you store a foreach in variabla and call it, it give undefined output
+const values = coding.forEach( (item) => {
+    console.log(item);
+    return item;
+})
+console.log(values); // undefined
+
+*/
+/*
+coding.forEach( (item) => {
+    console.log(item);
+    
+})
+
+*/
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+/*
+const newNums = myNums.filter( (num) => num > 4 )
+console.log(newNums);
+*/
+
+/*
+// when you use curly brases you must use " return "
+const newNums = myNums.filter( (num) => {
+    num > 4 // doesn't return value
+    return num > 4
+})
+console.log(newNums);
+*/
+
+const newNums = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+})
+console.log(newNums);
