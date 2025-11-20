92da02510c71ac6efd580626a70622e1c5529ccc
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-11-20 11:10:06 +0000

    commit

---
+++ 09_advance_one/2Promises.js
@@ -0,0 +1,24 @@
+const promise1 = new Promise(function(resolve, reject) {
+    // D an async task
+    // DB calls, cryptography, networkcall
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    }, 1000)
+});
+
+promise1.then(function(){
+    console.log("Promise consumed");
+    
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+        
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 revolved");
+    
+})
