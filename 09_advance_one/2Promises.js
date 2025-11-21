a4387c67e1a95e01f96a7263bbc329c83bffbc67
Abhishek S Hooli <abhishekshooli101@gmail.com>
2025-11-21 06:43:36 +0000

    promise

--- 09_advance_one/2Promises.js
+++ 09_advance_one/2Promises.js
@@ -22,3 +22,91 @@ new Promise(function(resolve, reject){
     console.log("Async 2 resolved");
     
 })
+
+const promise3 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "chai", email: "chai@ex.com"})
+    }, 1000)
+})
+
+promise3.then(function(user){
+    console.log(user);
+})
+
+const promise4 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        // let error = false
+        if (!error){
+            resolve({username: "Abhi", password: "123"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+
+    }, 1000)
+})
+
+const username = promise4.then((user) => {
+    console.log(user);
+    return user.username
+    
+}).then((username) => {
+    console.log(username);
+    
+}).catch((error) => {
+    console.log(error);
+    
+}).finally(() => {
+    console.log("The promise is either resolved or rejected");
+    
+})
+
+
+const promise5 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        // let error = false
+        if (!error){
+            resolve({username: "javascript", password: "123"})
+        }else{
+            reject('ERROR: js went wrong')
+        }
+
+    }, 1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promise5
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+consumePromiseFive()
+
+// async function getAllUsers(){
+//    try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     
+//    } catch (error) {
+//         console.log("E", error);
+//          
+//   }
+// }
+
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(function(response){
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+    
+})
+.catch((error) => console.log(error));
